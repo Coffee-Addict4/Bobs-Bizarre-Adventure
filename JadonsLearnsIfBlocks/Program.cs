291d52e434289e160a1a789029a647b3d3d8f942
Coffee-Addict4 <jadonconnelly2002@gmail.com>
2022-08-15 20:44:57 -0500

    DAD helped me out
    Dad really did most of the work, I need to do better.

---
+++ JadonsLearnsIfBlocks/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("I am the lorax I Speak for the Trees]");
+
+
+var response = Console.ReadLine();
+
+if(response == "Y" || response == "y")
+{
+    Console.WriteLine("Yes");
+}
+else if(response == "N")
+{
+    Console.WriteLine("No");
+}
+else
+{
+    Console.WriteLine("Unknown");
+}
+
+// Awnser result = QuestionHelper.AskQuestion("do you get up?");
+
+
+// internal static class QuestionHelper
+// {
+//     public static Awnser AskQuestion(string question)
+//     {
+//         //TODO: Jadon needs to write function that asks a question
+//         // And only returns a yes or no based on user Input
+//     }
+// }
+
+// internal class Question
+// {
+//     public string value { get; set; }
+
+// }
+
+// internal enum Awnser : int
+// {
+//     Unknown,
+
+//     yes,
+
+//     no
+// }
